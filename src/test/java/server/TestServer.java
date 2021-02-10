a8de5f1d0e1e373784f9b165a8fde5dc37113127
Philip Andersson <phander@kth.se>
2021-02-10 14:33:00 +0100

    Add test skeleton

---
+++ src/test/java/server/TestServer.java
@@ -0,0 +1,14 @@
+package server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestServer {
+    
+    @Test
+    public void dummyTest() {
+        int a = 1;
+        assertEquals(a,1);
+    }
+}
