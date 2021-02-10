a382fc9952518f0ac5e3298a9adfd8edea348679
Philip Andersson <phander@kth.se>
2021-02-10 14:45:27 +0100

    Add dummy test that uses server object #7

--- src/test/java/server/TestServer.java
+++ src/test/java/server/TestServer.java
@@ -4,11 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+// Run Maven tests: mvn test
+
 public class TestServer {
     
     @Test
     public void dummyTest() {
         int a = 1;
         assertEquals(a,1);
+    }
+
+    @Test
+    public void dummyTest2() {
+        ContinuousIntegrationServer server = new ContinuousIntegrationServer();
+        int a = server.dummyFunction();
+        assertEquals(1,a);
     }
 }
