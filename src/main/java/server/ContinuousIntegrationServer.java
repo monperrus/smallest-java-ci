cdbc39f98337f86707271850834e5df390f95d8a
Elisabet Lövkvist <elisabet@squeeee.se>
2021-02-10 22:05:32 +0100

    Wrapped request handling in if statement so it only runs if github push

--- src/main/java/server/ContinuousIntegrationServer.java
+++ src/main/java/server/ContinuousIntegrationServer.java
@@ -36,66 +36,69 @@ public class ContinuousIntegrationServer extends AbstractHandler
         baseRequest.setHandled(true);
 
         System.out.println(target);
-        // here you do all the continuous integration tasks
-        // for example
-        // 1st clone your repository
-        // 2nd compile the code
+
         String who = request.getHeader("user-agent");
-        String buildOK = "build not done";
-        String notifyOK = "notification not sent";
 
-        String URL = getRepoURL();
-        String cloneOK = cloneRepo(URL);
+        if(who.contains("GitHub-Hookshot")){
+          String what = request.getHeader("X-GitHub-Event");
+          if(what.contains("push")){
+          String buildOK = "build not done";
+          String notifyOK = "notification not sent";
+
+          String URL = getRepoURL();
+          String cloneOK = cloneRepo(URL);
 
 
-        if(cloneOK.contains("Cloning OK")){
-          buildOK = buildAndTest("path");
+          if(cloneOK.contains("Cloning OK")){
+            buildOK = buildAndTest("path");
+          }
+
+          if(buildOK.contains("Build OK")){
+            notifyOK = notify(buildOK);
+          }
+
+          System.out.println("Request handled");
+
+          if(notifyOK.contains("Notification sent successfully")){
+            System.out.println(notifyOK);
+          }
+
+        /*  if(who.contains("GitHub-Hookshot")){ //this branch is called if the request is a webhook
+              //this reads the body of the webhook as a string that is formatted as a json
+              BufferedReader br = request.getReader();
+              String str;
+              StringBuilder wholeStr = new StringBuilder();
+              while ((str = br.readLine()) != null) {
+                  wholeStr.append(str);
+              }
+              String ss = wholeStr.toString();
+              //this interprets the string as a json object so that it's parameters can be pulled
+              com.alibaba.fastjson.JSONObject jsonObject = com.alibaba.fastjson.JSONObject.parseObject(ss);
+              //this extracts the branch in which the event occurred as lastOne
+              String refs = jsonObject.get("ref").toString();
+              String[] sss = refs.split("/");
+              String lastOne = sss[sss.length - 1];
+              //this extracts the url of the repository where the event occurred as git_url
+              String git_url = jsonObject.getJSONObject("repository").get("git_url").toString();
+              //Process p1 = Runtime.getRuntime().exec("cd C:\\Users\\Kalle\\git\\cloneplace");
+              String git_url_fixed = git_url.replaceFirst("git", "https");
+              //this clones the specified branch of the specified repository to the folder specified in folder_path
+              String folder_path = " C:\\Users\\Kalle\\git\\cloneplace";
+              Process p = Runtime.getRuntime().exec("git clone -b" + " " + lastOne + " " + git_url_fixed + folder_path);
+
+              //this I don't quite know what it does
+              InputStream fis = p.getInputStream();
+              InputStreamReader isr = new InputStreamReader(fis);
+              BufferedReader fg = new BufferedReader(isr);
+              String line = null;
+              //System.out.println("git clone -b" + " " + lastOne + " " + git_url_fixed + " ..\\new");
+              while ((line = fg.readLine()) != null) {
+                  System.out.println(line);
+                  response.getWriter().println(line);
+              }
+          } */
         }
-
-        if(buildOK.contains("Build OK")){
-          notifyOK = notify(buildOK);
-        }
-
-        System.out.println("Request handled");
-
-        if(notifyOK.contains("Notification sent successfully")){
-          System.out.println(notifyOK);
-        }
-
-      /*  if(who.contains("GitHub-Hookshot")){ //this branch is called if the request is a webhook
-            //this reads the body of the webhook as a string that is formatted as a json
-            BufferedReader br = request.getReader();
-            String str;
-            StringBuilder wholeStr = new StringBuilder();
-            while ((str = br.readLine()) != null) {
-                wholeStr.append(str);
-            }
-            String ss = wholeStr.toString();
-            //this interprets the string as a json object so that it's parameters can be pulled
-            com.alibaba.fastjson.JSONObject jsonObject = com.alibaba.fastjson.JSONObject.parseObject(ss);
-            //this extracts the branch in which the event occurred as lastOne
-            String refs = jsonObject.get("ref").toString();
-            String[] sss = refs.split("/");
-            String lastOne = sss[sss.length - 1];
-            //this extracts the url of the repository where the event occurred as git_url
-            String git_url = jsonObject.getJSONObject("repository").get("git_url").toString();
-            //Process p1 = Runtime.getRuntime().exec("cd C:\\Users\\Kalle\\git\\cloneplace");
-            String git_url_fixed = git_url.replaceFirst("git", "https");
-            //this clones the specified branch of the specified repository to the folder specified in folder_path
-            String folder_path = " C:\\Users\\Kalle\\git\\cloneplace";
-            Process p = Runtime.getRuntime().exec("git clone -b" + " " + lastOne + " " + git_url_fixed + folder_path);
-
-            //this I don't quite know what it does
-            InputStream fis = p.getInputStream();
-            InputStreamReader isr = new InputStreamReader(fis);
-            BufferedReader fg = new BufferedReader(isr);
-            String line = null;
-            //System.out.println("git clone -b" + " " + lastOne + " " + git_url_fixed + " ..\\new");
-            while ((line = fg.readLine()) != null) {
-                System.out.println(line);
-                response.getWriter().println(line);
-            }
-        } */
+      }
     }
 
     public int dummyFunction() {
