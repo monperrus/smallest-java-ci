2971f89afeece00bd58635456f95d586d1bb4717
jonte450 <52628992+jonte450@users.noreply.github.com>
2021-02-12 19:49:10 +0100

    Update ContinuousIntegrationServer.java
    Testing removing some httpclient import again

--- src/main/java/server/ContinuousIntegrationServer.java
+++ src/main/java/server/ContinuousIntegrationServer.java
@@ -26,7 +26,6 @@ import org.apache.http.impl.client.HttpClientBuilder;
 
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
