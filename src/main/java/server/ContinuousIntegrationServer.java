cc440b34486876118e5365fa35b8e8478fa1cf8e
jonte450 <52628992+jonte450@users.noreply.github.com>
2021-02-12 20:46:18 +0100

    Update ContinuousIntegrationServer.java
    testing

--- src/main/java/server/ContinuousIntegrationServer.java
+++ src/main/java/server/ContinuousIntegrationServer.java
@@ -25,6 +25,13 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
+import org.apache.http.impl.client.CloseableHttpClient;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 
 //
 import org.apache.http.Header;
