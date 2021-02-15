9813c20d45bf60abeb8d8444260dd8e9f06bd97e
jonte450 <52628992+jonte450@users.noreply.github.com>
2021-02-15 11:48:37 +0100

    Update TestServer.java
    Changes

--- src/test/java/server/TestServer.java
+++ src/test/java/server/TestServer.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.*;
+import org.json.*;
+
 // Run Maven tests: mvn test
 
 public class TestServer {
