43693557c0c9dbd2a4a262e3f30246e10de94638
Elisabet Lövkvist <elisabet@squeeee.se>
2021-02-17 17:04:00 +0100

    Changed test_getJSON to use the help_getJSON function

--- src/test/java/server/TestServer.java
+++ src/test/java/server/TestServer.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static server.ContinuousIntegrationServer.getJSON;
+import static server.ContinuousIntegrationServer.getRepoURL;
+import static server.ContinuousIntegrationServer.getStatusUrl;
 
 import java.io.*;
 import org.json.*;
@@ -11,7 +13,7 @@ import server.ContinuousIntegrationServer.*;
 // Run Maven tests: mvn test
 
 public class TestServer {
-    
+
     @Test
     public void dummyTest() {
         int a = 1;
@@ -20,33 +22,10 @@ public class TestServer {
 
     @Test
     public void test_getJSON(){
-        BufferedReader inputString = null;
-        BufferedReader inputString2 = null;
-
-        {
-            try {
-                inputString = new BufferedReader(new FileReader("src/test/java/server/test2.json"));
-                inputString2 = new BufferedReader(new FileReader("src/test/java/server/test3.json"));
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
-        JSONObject json = null;
-        JSONObject json2 = null;
-
-        {
-            try {
-                assert inputString != null;
-                assert inputString2 != null;
-                json = getJSON(inputString);
-                json2 = getJSON(inputString2);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
+        //True case: expected JSON file (webhook response from github), should return a non-empty JSON object
+        //False: file with JSON that is empty, should return an empty JSON object.
+        JSONObject json = help_getJSON("src/test/java/server/test2.json");
+        JSONObject json2 = help_getJSON("src/test/java/server/test3.json");
 
         assertEquals(json.isEmpty(), false);
         assertEquals(json2.isEmpty() ,true);
@@ -55,30 +34,34 @@ public class TestServer {
 
     @Test
     public void test_getRepoURL(){
-    //Test True    
-    JSONObject json_true = new JSONObject();
-    JSONObject git_url = new JSONObject();
-    git_url.put("git_url","https://github.com/DD2480-Group-15/ci-server");
-    json_true.put("repository", git_url);
-    json_true.put("ref","/tree/issue/22");
-    String true_return = "tree/issue/22" +""+"git://github.com/DD2480-Group-15/ci-server";
-    //assertEquals(getRepoURL(json_true), true_return);
-    
-    //Test False
-    JSONObject json_false = new JSONObject();
-    JSONObject git_url_1 = new JSONObject();
-    git_url_1.put("git_url","/tree/issue/22");
-    json_false.put("repository", git_url_1);
-    json_false.put("ref","https://github.com/DD2480-Group-15/ci-server");
-    String false_return = "tree/issue/22" +""+"git://github.com/DD2480-Group-15/ci-server";
-    //assertNotEquals(getRepoURL(json_false), false_return); 
+      //Test True
+      JSONObject json_true = new JSONObject();
+      JSONObject git_url = new JSONObject();
+      git_url.put("git_url","https://github.com/DD2480-Group-15/ci-server");
+      json_true.put("repository", git_url);
+      json_true.put("ref","/tree/issue/22");
+      String true_return = "tree/issue/22" +""+"git://github.com/DD2480-Group-15/ci-server";
+      //assertEquals(getRepoURL(json_true), true_return);
+
+      //Test False
+      JSONObject json_false = new JSONObject();
+      JSONObject git_url_1 = new JSONObject();
+      git_url_1.put("git_url","/tree/issue/22");
+      json_false.put("repository", git_url_1);
+      json_false.put("ref","https://github.com/DD2480-Group-15/ci-server");
+      String false_return = "tree/issue/22" +""+"git://github.com/DD2480-Group-15/ci-server";
+      //assertNotEquals(getRepoURL(json_false), false_return);
     }
 
     @Test
     public void test_getStatusUrl(){
+        JSONObject json_true = help_getJSON("src/test/java/server/testdata_getstatusurl_T.json");
+        JSONObject json_false = help_getJSON("src/test/java/server/testdata_getstatusurl_F.json");
+
+      //  String statusUrl_true = getStatusUrl(json_true);
+        //String statusUrl_false = getStatusUrl(json_false);
 
     }
-
    //@Test
    //public void test(){
 
@@ -88,9 +71,36 @@ public class TestServer {
    //public void test(){
 
    //}
-    
 
 
+   public static JSONObject help_getJSON(String filepath){
+     //helper to get JSON object from a filename
+
+     BufferedReader inputFromFile = null;
+
+     {
+         try {
+             inputFromFile = new BufferedReader(new FileReader(filepath));
+
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         }
+     }
+
+     JSONObject json = null;
+
+     {
+         try {
+             assert inputFromFile != null;
+             json = getJSON(inputFromFile);
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+
+     return json;
+
+   }
 
     @Test
     public void dummyTest2() {
