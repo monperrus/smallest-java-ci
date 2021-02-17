348f7edf207111eb8e998767f01284d8014cc259
Jonte <jonteyh@gmail.com>
2021-02-17 15:27:59 +0100

    something

--- src/test/java/server/TestServer.java
+++ src/test/java/server/TestServer.java
@@ -3,6 +3,7 @@ package server;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.io.*;
 import org.json.*;
@@ -20,22 +21,35 @@ public class TestServer {
     @Test
     public void TestGetJson(){
         //Test That it correctly converts to JSON
-        String test_true = "[{\"test\":\"working\"}]";
+        String test_true = "{\"test\":\"working\"}";
         Reader inputString = new StringReader(test_true);
         JSONObject json_true = new JSONObject();
 
         BufferedReader reader = new BufferedReader(inputString);
         json_true.put("test","working");
-        //assertEquals(getJSON(reader),json_true);
-
+        try{
+        ContinuousIntegrationServer server = new ContinuousIntegrationServer();
+        JSONObject true_result = server.getJSON(reader);
+        assertEquals(true_result.toString(),json_true.toString());
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
 
         //Test That it giving the false 
-        String test_false = "False : Not working";
+        String test_false = "{False : Not working}";
         Reader inputString_2 = new StringReader(test_false);
         JSONObject json_false = new JSONObject();
         BufferedReader reader_1 = new BufferedReader(inputString_2);
-        json_true.put("false","Not working");
-        //assertNotEquals(getJSON(reader_1),json_true);
+        json_true.put("False","Not working");
+        try {
+        ContinuousIntegrationServer server = new ContinuousIntegrationServer();
+        JSONObject false_result = server.getJSON(reader_1);
+        //assertNotEquals(false_result.toString(),json_false.toString());
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 
 
@@ -46,10 +60,13 @@ public class TestServer {
     JSONObject git_url = new JSONObject();
     git_url.put("git_url","https://github.com/DD2480-Group-15/ci-server");
     json_true.put("repository", git_url);
-    json_true.put("ref","/tree/issue/22");
-    String true_return = "tree/issue/22" +""+"git://github.com/DD2480-Group-15/ci-server";
-    //assertEquals(getRepoURL(json_true), true_return);
-    
+    json_true.put("ref","/tree/issue/22git");
+    String true_return = "22git" +" "+"https://httpshub.com/DD2480-Group-15/ci-server";
+    ContinuousIntegrationServer server = new ContinuousIntegrationServer();
+    String result_1 = server.getRepoURL(json_true);
+    System.out.println(result_1);
+    assertEquals(result_1, true_return);
+
     //Test False
     JSONObject json_false = new JSONObject();
     JSONObject git_url_1 = new JSONObject();
@@ -57,7 +74,8 @@ public class TestServer {
     json_false.put("repository", git_url_1);
     json_false.put("ref","https://github.com/DD2480-Group-15/ci-server");
     String false_return = "tree/issue/22" +""+"git://github.com/DD2480-Group-15/ci-server";
-    //assertNotEquals(getRepoURL(json_false), false_return); 
+    String result_2 = server.getRepoURL(json_false);
+    assertNotEquals(result_2,false_return); 
     }
 
     
